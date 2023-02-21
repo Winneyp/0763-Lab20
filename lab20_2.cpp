d5eb5933c00ae17d88b6c3dd6a915b99e4ef9ae1
P <tatakornaow@gmail.com>
2023-02-21 16:54:30 +0700

    20.1-.2

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -5,8 +5,24 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *r1, Rect *r2){
+	double upperY = min(r1->y,r2->y);
+	r1->y -= r1->h;
+	r2->y -= r2->h;
+	double lowerY = max(r1->y,r2->y);
 
+	double leftX = max(r1->x,r2->x);
+	r1->x += r1->w;
+	r2->x += r2->w;
+	double rightX = min(r1->x,r2->x);
 
+	double wigth = rightX - leftX;
+	double hight = upperY - lowerY;
+
+	double area = hight*wigth; 
+	
+
+	if (area<=0) return area = 0;
+	return area;
 
 }
